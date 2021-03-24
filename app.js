cd12250a08a10d5761f107898be33fcc225b703a
felix24mihai <mihai_prava@yahoo.com>
2021-03-24 02:13:28 +0200

    Restful API

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+require('dotenv/config');
 
 //Init app
 const app = express();
@@ -6,7 +10,25 @@ const app = express();
 //Start server
 app.listen(3000);
 
+//Body Parser Middleware
+app.use(bodyParser.urlencoded({extended: false}))
+
+//Parse application/json
+app.use(bodyParser.json());
+
+app.use(cors());
+
+//Connect to DB
+mongoose.connect(
+    process.env.DB_CONNECTION, { useNewUrlParser: true, useUnifiedTopology: true }, () => 
+    console.log('Connected to DB!')
+);
+
+//Import routes
+const usersRoute = require('./routes/users');
+app.use('/users', usersRoute);
+
 //Home route
 app.get('/', (req, res) => {
-    res.send('Test!!!')
+    res.send('Test!!!');
 });
