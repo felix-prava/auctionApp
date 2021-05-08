61cdbb98e2d9081ae44a4263665862d7a80b53ff
madalin11 <madalin.c.puiu@gmail.com>
2021-05-08 21:12:05 +0300

    Add filters controller

---
+++ server/Database/controllers/filters.js
@@ -0,0 +1,7 @@
+const filters = {
+    "transmission": "car_details.car_specifications.transmission_type",
+    "fuel_type": "car_details.car_specifications.fuel_type",
+    "model": "title.car_model",
+
+
+}
